1d68d999660edebe2ea612729796bbe0558edc90
hstone1 <henry_stone1@brown.edu>
2017-04-07 14:29:21 -0400

    ts chat and player

---
+++ src-frontend/app/game/models/player.model.ts
@@ -0,0 +1,19 @@
+export class Player {
+  public victoryPoints:number;
+  public actions:number;
+  public buys:number;
+  public gold:number;
+
+  constructor(
+    public id:number,
+    public color:string,
+    public name:string) {
+
+    this.victoryPoints = 3;
+    this.buys = 0;
+    this.actions = 0;
+    this.gold = 0;
+  }
+
+  drawHand() {}
+}
