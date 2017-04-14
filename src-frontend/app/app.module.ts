248f5b75b9f2aaacfce17ae1e57178ab012cc79c
Brendan Le <le.brendan@gmail.com>
2017-04-14 11:21:41 -0400

    Add routing

--- src-frontend/app/app.module.ts
+++ src-frontend/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {LobbyComponent} from "./lobby/lobby.component";
@@ -12,30 +11,7 @@ import {GameComponent} from "./game/game.component";
 import {UserIdService} from "./shared/user-id.service";
 import {GameService} from "./game/game.service";
 import {SocketService} from "./shared/socket.service";
-
-const appRoutes: Routes = [
-  {
-    path: 'lobby',
-    component: LobbyComponent
-  },
-  {
-    path: 'game',
-    component: GameComponent
-  },
-  {
-    path: 'create',
-    component: CreationComponent
-  },
-  { path: '',
-    redirectTo: '/lobby',
-    pathMatch: 'full'
-  },
-  {
-    path: '**',
-    component: LobbyComponent
-  }
-];
-
+import {appRoutes} from "./routes";
 
 @NgModule({
   declarations: [
