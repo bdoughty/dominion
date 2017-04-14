2bf130816f7ca5e8572e3c6917c0e109a3b9f2dc
Brendan Le <le.brendan@gmail.com>
2017-04-14 11:21:28 -0400

    Add routes

--- src-frontend/app/app.module.ts
+++ src-frontend/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import {LobbyComponent} from "./lobby/lobby.component";
@@ -10,6 +12,30 @@ import {GameComponent} from "./game/game.component";
 import {UserIdService} from "./shared/user-id.service";
 import {GameService} from "./game/game.service";
 import {SocketService} from "./shared/socket.service";
+
+const appRoutes: Routes = [
+  {
+    path: 'lobby',
+    component: LobbyComponent
+  },
+  {
+    path: 'game',
+    component: GameComponent
+  },
+  {
+    path: 'create',
+    component: CreationComponent
+  },
+  { path: '',
+    redirectTo: '/lobby',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: LobbyComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
@@ -25,7 +51,8 @@ import {SocketService} from "./shared/socket.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     UserIdService,
