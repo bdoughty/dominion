63d1d78292d814c0836f3473d628a7d63affa881
bdoughty <benjamin.doughty@gmail.com>
2017-04-15 11:51:50 -0400

    Cleaned up Board, removing methods for buying individual cards and instead allowing you to buy cards based on id alone

--- src/main/java/edu/brown/cs/dominion/gameutil/Board.java
+++ src/main/java/edu/brown/cs/dominion/gameutil/Board.java
@@ -1,34 +1,22 @@
 package edu.brown.cs.dominion.gameutil;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import edu.brown.cs.dominion.Card;
 import edu.brown.cs.dominion.money.Copper;
 import edu.brown.cs.dominion.money.Gold;
-import edu.brown.cs.dominion.money.Money;
 import edu.brown.cs.dominion.money.Silver;
 import edu.brown.cs.dominion.victory.Curse;
 import edu.brown.cs.dominion.victory.Duchy;
 import edu.brown.cs.dominion.victory.Estate;
 import edu.brown.cs.dominion.victory.Province;
-import edu.brown.cs.dominion.victory.VictoryPoint;
 
 public class Board {
 
-  private Pile coppers;
-  private Pile silvers;
-  private Pile golds;
-
-  private Pile estates;
-  private Pile duchies;
-  private Pile provinces;
-  private Pile curses;
-
-  private List<Pile> actions;
-
-  private List<Pile> allPiles;
+  private Map<Integer, Pile> piles;
 
   private List<Card> trash;
 
@@ -38,82 +26,51 @@ public class Board {
   }
 
   private void boardSetUp(List<Integer> actionCardIds) {
-    this.coppers = new Pile(Copper::new, 60);
-    this.silvers = new Pile(Silver::new, 40);
-    this.golds = new Pile(Gold::new, 30);
+    piles = new HashMap<>();
 
-    this.estates = new Pile(Estate::new, 24);
-    this.duchies = new Pile(Duchy::new, 12);
-    this.provinces = new Pile(Province::new, 12);
-    this.curses = new Pile(Curse::new, 30);
+    // building board
+    // money
+    piles.put(0, new Pile(Copper::new, 60));
+    piles.put(1, new Pile(Silver::new, 40));
+    piles.put(2, new Pile(Gold::new, 30));
 
-    this.actions = new ArrayList<>();
-    for (int i = 0; i < 10; i++) {
-      this.actions.add(new Pile(
-          CardFactory.getInstance().getFactory(actionCardIds.get(i)), 10));
+    // victory points
+    piles.put(3, new Pile(Estate::new, 24));
+    piles.put(4, new Pile(Duchy::new, 12));
+    piles.put(5, new Pile(Province::new, 12));
+    piles.put(6, new Pile(Curse::new, 30));
+
+    // actions
+    for (int i = 0; i < actionCardIds.size(); i++) {
+      int id = actionCardIds.get(i);
+      piles.put(id, new Pile(CardFactory.getInstance().getFactory(id), 10));
     }
 
-    this.allPiles.addAll(this.actions);
-    this.allPiles.add(this.coppers);
-    this.allPiles.add(this.silvers);
-    this.allPiles.add(this.golds);
-    this.allPiles.add(this.estates);
-    this.allPiles.add(this.duchies);
-    this.allPiles.add(this.provinces);
-    this.allPiles.add(this.curses);
-
-    this.trash = new LinkedList<>();
+    trash = new LinkedList<>();
   }
 
-  public Card buyMoney(Money m) {
-    switch (m) {
-      case COPPER:
-        return this.coppers.draw();
-      case SILVER:
-        return this.silvers.draw();
-      case GOLD:
-        return this.golds.draw();
-      default:
-        throw new UnsupportedOperationException(
-            "can't buy money of this type!");
+  public Card buyCard(int id) {
+    if (piles.containsKey(id)) {
+      return piles.get(id).draw();
+    } else {
+      throw new IllegalArgumentException("No card with id: " + id);
     }
-  }
-
-  public Card buyVictoryPoint(VictoryPoint vp) {
-    switch (vp) {
-      case ESTATE:
-        return this.estates.draw();
-      case DUCHY:
-        return this.duchies.draw();
-      case PROVINCE:
-        return this.provinces.draw();
-      case CURSE:
-        return this.curses.draw();
-      default:
-        throw new UnsupportedOperationException(
-            "can't buy victory point of this type!");
-    }
-  }
-
-  public Card buyAction(int i) {
-    assert (i >= 0 && i < 10);
-    return this.actions.get(i).draw();
   }
 
   public void trashCard(Card c) {
-    this.trash.add(c);
+    trash.add(c);
   }
 
   public boolean gameHasEnded() {
     int emptyPiles = 0;
 
-    for (Pile p : this.allPiles) {
+    for (Pile p : piles.values()) {
       if (p.isEmpty()) {
         emptyPiles++;
       }
     }
 
-    return this.provinces.isEmpty() || emptyPiles >= 3;
+    return piles.get(5).isEmpty() || emptyPiles >= 3;
   }
 
 }
