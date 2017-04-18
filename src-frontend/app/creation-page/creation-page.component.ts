ee66e566d3ae44b5258f456b2ee6f04a63574884
Solomonz <solomonzitter@gmail.com>
2017-04-18 18:33:55 -0400

    can now select cards to start a game with. almost ready to make the form submittable

--- src-frontend/app/creation-page/creation-page.component.ts
+++ src-frontend/app/creation-page/creation-page.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'dmn-creation',
   templateUrl: './creation-page.component.html',
   styleUrls: ['./creation-page.component.css']
-}) export class CreationComponent implements OnInit {
-  title = 'app works!';
-  public blowUp = document.getElementById("blowUp");
+}) export class CreationComponent {
+  private selected = new Set();
 
-  ngOnInit() {
-    console.log("hello");
-    // for(let i = 0; i < 20; i++) {
-    //   document.getElementById("card" + i).addEventListener("onmouseenter", event => {
-    //     this.blowUp.innerHTML = "<img class='image' id='blowUpImg' src='../../assets/card" + i + ".jpg' />";
-    //     document.getElementById("blowUpImg").style.visibility = "visible";
-    //     console.log("mouse entered " + i);
-    //   });
-    //
-    //   document.getElementById("card" + i).addEventListener("onmouseleave", event => {
-    //     document.getElementById("blowUpImg").style.visibility = "hidden";
-    //   });
-    // }
+  entered(cardId) {
+    console.log("entered " + cardId);
+    document.getElementById("blowUp").innerHTML = "<img id='blowUpImg' src='../../assets/" + cardId + ".jpg' " +
+      "style='visibility: visible; width: auto; height: 100%;' />";
+  }
+
+  left() {
+    console.log("left");
+    document.getElementById("blowUpImg").style.visibility = "hidden";
+  }
+
+  clicked(cardId) {
+    if(this.selected.has(cardId)) {
+      this.selected.delete(cardId);
+      document.getElementById(cardId).style.border = "0";
+    } else {
+      this.selected.add(cardId);
+      document.getElementById(cardId).style.border = "3px solid yellow";
+    }
+  }
+
+  submitForm() {
+
   }
 }
 
