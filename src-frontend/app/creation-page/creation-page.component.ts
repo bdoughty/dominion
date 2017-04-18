870e78a4e2f9c5fb615e8b51b25132f513a656e4
Solomonz <solomonzitter@gmail.com>
2017-04-18 13:49:39 -0400

    trying to add the event listeners for the cards on the creation page, not working yet. got the chat to not resize vertically on the creation page

--- src-frontend/app/creation-page/creation-page.component.ts
+++ src-frontend/app/creation-page/creation-page.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'dmn-creation',
   templateUrl: './creation-page.component.html',
   styleUrls: ['./creation-page.component.css']
-}) export class CreationComponent {
+}) export class CreationComponent implements OnInit {
   title = 'app works!';
+  public blowUp = document.getElementById("blowUp");
 
+  ngOnInit() {
+    console.log("hello");
+    for(let i = 0; i < 20; i++) {
+      document.getElementById("card" + i).addEventListener("onmouseenter", event => {
+        this.blowUp.innerHTML = "<img class='image' id='blowUpImg' src='../../assets/card" + i + ".jpg' />";
+        document.getElementById("blowUpImg").style.visibility = "visible";
+        console.log("mouse entered " + i);
+      });
+
+      document.getElementById("card" + i).addEventListener("onmouseleave", event => {
+        document.getElementById("blowUpImg").style.visibility = "hidden";
+      });
+    }
+  }
 }
 
-let blowUp = document.getElementById("blowUp");
 
-function hoveringOver(cardId) {
-  this.blowUp.innerHTML = "<img class='image' src='../../assets/card" + cardId + ".jpg' />";
-}
