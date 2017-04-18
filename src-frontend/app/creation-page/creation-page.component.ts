69bb39dbf6060bade502836eeefa2815f93fc672
Solomonz <solomonzitter@gmail.com>
2017-04-18 12:59:32 -0400

    got a good structure for the creation page up

--- src-frontend/app/creation-page/creation-page.component.ts
+++ src-frontend/app/creation-page/creation-page.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./creation-page.component.css']
 }) export class CreationComponent {
   title = 'app works!';
+
+}
+
+let blowUp = document.getElementById("blowUp");
+
+function hoveringOver(cardId) {
+  this.blowUp.innerHTML = "<img class='image' src='../../assets/card" + cardId + ".jpg' />";
 }
