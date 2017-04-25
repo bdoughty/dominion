54ffba9fa0b64d755a00614c3576304021848517
hstone1 <henry_stone1@brown.edu>
2017-04-25 17:45:05 -0400

    sending pile sizes

--- src/main/java/edu/brown/cs/dominion/gameutil/Pile.java
+++ src/main/java/edu/brown/cs/dominion/gameutil/Pile.java
@@ -7,7 +7,7 @@ import edu.brown.cs.dominion.Card;
 public class Pile {
   private int size;
   private transient Supplier<Card> card;
-  private Card c;
+  private transient Card c;
 
   public Pile(Supplier<Card> card, int size) {
     this.size = size;
@@ -43,7 +43,7 @@ public class Pile {
   public int getCost() {
     return c.getCost();
   }
-  
+
   public int getId() {
     return c.getId();
   }
