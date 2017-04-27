e25ac08a8052d54229b057a89c9e690961e6cb28
Brendan Le <le.brendan@gmail.com>
2017-04-26 20:10:55 -0400

    Revert "cellar stops after last card is selected"
    This reverts commit 581e93b9fae85fc6723c98d65b10877ea1d067e3.

--- src/main/java/edu/brown/cs/dominion/action/Cellar.java
+++ src/main/java/edu/brown/cs/dominion/action/Cellar.java
@@ -49,17 +49,11 @@ class DiscardOne implements SelectCallback {
     cm.discardPileSize(g.getPlayerFromUser(u).getDiscard().size());
     cm.piles(g.getBoard());
 
-    if(g.getPlayerFromUser(u).getHand().size() > 0){
-      cm.requireSelectCanStop(u,
+    cm.requireSelectCanStop(u,
         g.getPlayerFromUser(u).getHand().stream().map(Card::getId)
-          .collect(Collectors.toList()),
+            .collect(Collectors.toList()),
         ImmutableList.<Integer> of(), new DiscardOne(g, discarded + 1),
         new CellarDraw(g, discarded + 1));
-    } else {
-      new CellarDraw(g, discarded + 1).cancel();
-    }
-    g.playerUpdateMap(cm, g.getCurrentPlayer());
-
 
     return cm;
   }
