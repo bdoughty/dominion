9773a0a1e1a89a805715d065319516df0a4c7a63
bdoughty <benjamin.doughty@gmail.com>
2017-04-27 00:48:03 -0400

    AIPlayer Skeleton

---
+++ src/main/java/edu/brown/cs/dominion/AIPlayer.java
@@ -0,0 +1,25 @@
+package edu.brown.cs.dominion;
+
+import edu.brown.cs.dominion.games.Game;
+import edu.brown.cs.dominion.io.send.Callback;
+
+public class AIPlayer extends User implements AI {
+
+  public AIPlayer(int id) {
+    super(id);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public void play(Game g) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void doCallback(Game g, Callback c) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
