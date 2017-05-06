122324cffadbd61d55ad62907764fd04674f7fd4
Solomonz <solomonzitter@gmail.com>
2017-05-06 19:35:33 -0400

    added package declarations to make it compile

--- src/main/java/edu/brown/cs/dominion/neuralNetwork/NNH.java
+++ src/main/java/edu/brown/cs/dominion/neuralNetwork/NNH.java
@@ -1,6 +1,7 @@
+package edu.brown.cs.dominion.neuralNetwork;
 
 public class NNH {
-  public static double[] flatten(double[][] input){
+  public static double[] flatten(double[][] input) {
     return null;
   }
 }
