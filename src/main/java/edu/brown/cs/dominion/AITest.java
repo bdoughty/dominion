3d4d310d91c89fbca0f4f53fe1dc7ddc90f1efee
Solomonz <solomonzitter@gmail.com>
2017-05-07 22:29:53 -0400

    I don't remember what I changed

--- src/main/java/edu/brown/cs/dominion/AITest.java
+++ src/main/java/edu/brown/cs/dominion/AITest.java
@@ -15,8 +15,8 @@ import edu.brown.cs.dominion.games.Game;
 public class AITest {
   public static void main(String[] args) {
     for (int i = 0; i < 500; i++) {
-      Game g = new Game(ImmutableList.of(new AIPlayer(new DumbStrategy())),
-          get10cards());
+      Game g = new Game(ImmutableList.of(new AIPlayer(new DumbStrategy()),
+          new AIPlayer(new DumbStrategy())), get10cards());
       g.play();
     }
   }
